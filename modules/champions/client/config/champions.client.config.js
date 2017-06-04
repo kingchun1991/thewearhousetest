0b0be8b4363256f1a95ba1ca92eeca20d1a3f29a
Frank O <kingchun1991@hotmail.com>
2017-06-04 09:48:28 +0800

    fix mean bug

--- modules/champions/client/config/champions.client.config.js
+++ modules/champions/client/config/champions.client.config.js
@@ -1,32 +1,27 @@
-(function () {
-  'use strict';
+'use strict';
 
-  angular
-    .module('champions')
-    .run(menuConfig);
+angular
+  .module('champions')
+  .run(['Menus',function (menuService) {
+      // Set top bar menu items
+      menuService.addMenuItem('topbar', {
+        title: 'Brands',
+        state: 'champions',
+        type: 'dropdown',
+        roles: ['*']
+      });
 
-  menuConfig.$inject = ['menuService'];
+      // Add the dropdown list item
+      menuService.addSubMenuItem('topbar', 'champions', {
+        title: 'Champion',
+        state: 'champions.list'
+      });
 
-  function menuConfig(menuService) {
-    // Set top bar menu items
-    menuService.addMenuItem('topbar', {
-      title: 'Champions',
-      state: 'champions',
-      type: 'dropdown',
-      roles: ['*']
-    });
-
-    // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'champions', {
-      title: 'List Champions',
-      state: 'champions.list'
-    });
-
-    // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', 'champions', {
-      title: 'Create Champion',
-      state: 'champions.create',
-      roles: ['user']
-    });
-  }
-}());
+      // Add the dropdown create item
+      menuService.addSubMenuItem('topbar', 'champions', {
+        title: 'Create Champion',
+        state: 'champions.create',
+        roles: ['user']
+      });
+    }
+  ]);
