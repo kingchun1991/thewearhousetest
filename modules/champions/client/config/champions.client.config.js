4daf57836e26ef0ef7572b35da08c4d31d675ec9
Frank O <kingchun1991@hotmail.com>
2017-06-06 20:55:06 +0800

    fix indention problem

--- modules/champions/client/config/champions.client.config.js
+++ modules/champions/client/config/champions.client.config.js
@@ -1,27 +1,24 @@
 'use strict';
 
-angular
-  .module('champions')
-  .run(['Menus',function (menuService) {
-      // Set top bar menu items
-      menuService.addMenuItem('topbar', {
-        title: 'Brands',
-        state: 'champions',
-        type: 'dropdown',
-        roles: ['*']
-      });
+angular.module('champions').run(['Menus', function (menu) {
+  // Set top bar menu items
+  menu.addMenuItem('topbar', {
+    title: 'Brands',
+    state: 'champions',
+    type: 'dropdown',
+    roles: ['*']
+  });
 
-      // Add the dropdown list item
-      menuService.addSubMenuItem('topbar', 'champions', {
-        title: 'Champion',
-        state: 'champions.list'
-      });
+  // Add the dropdown list item
+  menu.addSubMenuItem('topbar', 'champions', {
+    title: 'Champion',
+    state: 'champions.list'
+  });
 
-      // Add the dropdown create item
-      menuService.addSubMenuItem('topbar', 'champions', {
-        title: 'Create Champion',
-        state: 'champions.create',
-        roles: ['user']
-      });
-    }
-  ]);
+  // Add the dropdown create item
+  menu.addSubMenuItem('topbar', 'champions', {
+    title: 'Create Champion',
+    state: 'champions.create',
+    roles: ['user']
+  });
+}]);
